d9fc4eedafa5ed7d3f5e040c25f0649deeb5a09e
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2023-03-26 01:37:46 +0530

    Update 07.c

--- 07.c
+++ 07.c
@@ -13,16 +13,16 @@ int main()
 
     printf("Enter second time period (HH MM SS) : ");
     scanf("%d%d%d",&t2.hour,&t2.minut,&t2.second);
-
-    if(t1.minut<t2.minut)
-    {
-        t1.hour-=1;
-        t1.minut+=60;
-    }
+    
     if(t1.second<t2.second)
     {
         t1.minut-=1;
         t1.second+=60;
+    }
+    if(t1.minut<t2.minut)
+    {
+        t1.hour-=1;
+        t1.minut+=60;
     }
     printf("\nDifference between two time period %d:%d:%d \n\n",t1.hour-t2.hour,t1.minut-t2.minut,t1.second-t2.second);
     return 0;
